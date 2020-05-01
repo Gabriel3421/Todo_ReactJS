69e5a90e1f7ff65037423db0ba0d3127cbc8e767
Gabriel3421 <gabrielsouza.n.s@hotmail.com>
2020-05-01 10:47:34 -0300

    todo list finalizado

---
+++ src/components/TechItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Proptypes from 'prop-types';
+
+function TechItem({ tech, onDelete }) {
+  return(
+    <li >{tech}<button onClick={onDelete} type="button">X</button></li>
+  );
+}
+TechItem.defaultProps = {
+  tech: 'Oculto',
+};
+TechItem.proptypes = {
+  tech: Proptypes.string,
+  onDelete: Proptypes.func.isRequired,
+};
+export default TechItem;
